39324a5e1b26d5254ebe2429a33733c9b0e40fb5
Maksym Krasnov <maksimkrasnov747@gmail.com>
2026-04-30 20:54:49 +0200

    Add error handling for products fetch, fix CI build job by providing keys on GitHub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { fetchProducts } from "@/services/productsApi";
 import ProductList from "@/components/ProductList/ProductList";
 
 export default async function Home() {
   const data = await fetchProducts();
+
+  if (!data || !data.products) {
+    return <main><p>Failed to load Products</p></main>
+  }
 
   return <ProductList products={data.products} />;
 }
